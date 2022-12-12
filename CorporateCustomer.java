ad9d969e43326a8351df1a6ef17eb7406a8df44d
Tunc Oncu <tunconcu2021@gmail.com>
2022-12-12 18:12:14 +0300

    Adding files learnnig OOP

---
+++ CorporateCustomer.java
@@ -0,0 +1,21 @@
+
+
+public class CorporateCustomer extends Customer {
+
+	private String corporateName;
+	private String corporateAdress;
+	public String getCorporateName() {
+		return corporateName;
+	}
+	public void setCorporateName(String corporateName) {
+		this.corporateName = corporateName;
+	}
+	public String getCorporateAdress() {
+		return corporateAdress;
+	}
+	public void setCorporateAdress(String corporateAdress) {
+		this.corporateAdress = corporateAdress;
+	}
+	
+	
+}
